0021e508eb2265707ce6c4eb2caf9038dd1900bb
v.tikhomirov <v.tikhomirov@whitewill.ru>
2023-04-21 18:32:43 +0300

    Обновление: доки

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -41,13 +41,13 @@ function Docs() {
             <div class="docs-wrapper">
                 <a href="https://ruscyberleague.ru/static/Reglament_Sorevnovaniya_RCL_2023.pdf" class="title_md">Регламент совернования РКЛ Сезон 2023</a>
                 <div class="caption-sm">3239 Кб</div>
-                <a href="https://ruscyberleague.ru/static/Disciplinarniy_reglament_RCL_2023.pdf" class="title_md">Регламент Соревнования РКЛ Сезон 2023 года</a>
+                <a href="https://ruscyberleague.ru/static/Disciplinarniy_reglament_RCL_2023.pdf" class="title_md">Дисциплинарный регламент РКЛ Сезон 2023</a>
                 <div class="caption-sm">650 Кб</div>
-                <a href="https://ruscyberleague.ru/static/Polojenie_o_disciplinarnom_komitete_RCL_2023.pdf" class="title_md">Положение о призовом фонде РКЛ Сезон 2023</a>
+                <a href="https://ruscyberleague.ru/static/Polojenie_o_disciplinarnom_komitete_RCL_2023.pdf" class="title_md">Положение о дисциплинарном комитете РКЛ Сезон 2023</a>
                 <div class="caption-sm">179 Кб</div>
-                <a href="https://ruscyberleague.ru/static/Polohenie_o_prizovom_fonde_RCL_2023.pdf" class="title_md">IESF Statutes 2021</a>
+                <a href="https://ruscyberleague.ru/static/Polohenie_o_prizovom_fonde_RCL_2023.pdf" class="title_md">Положение о призовом фонде РКЛ Сезон 2023</a>
                 <div class="caption-sm">2048 Кб</div>
-                <a href="https://ruscyberleague.ru/static/IESF_Statutes_2021.pdf" class="title_md">CSGO_Rulebook</a>
+                <a href="https://ruscyberleague.ru/static/IESF_Statutes_2021.pdf" class="title_md">IESF Statutes 2021</a>
                 <div class="caption-sm">726 Кб</div>
                 <a href="https://ruscyberleague.ru/static/CSGO_Rulebook.pdf" class="title_md">CSGO_Rulebook</a>
                 <div class="caption-sm">5801 Кб</div>
