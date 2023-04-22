2df6adcd4cd5be0f9180e6fdd100fff2b35811e1
v.tikhomirov <v.tikhomirov@whitewill.ru>
2023-04-22 12:30:34 +0300

    Обновление: таблица ладдера м(моб),  блок новостей (моб)

--- src/components/tournamentTable.js
+++ src/components/tournamentTable.js
@@ -6,7 +6,7 @@ export default class TournamentTable extends React.Component {
         return (<div class="ladder-page-wrapper animate__animated animate__fadeIn">
             <div class="title_lg dark">ТУРНИРНАЯ ТАБЛИЦА</div>
             <table class="ladder_container">
-                <thead><tr><td>МЕСТО</td><td>КОМАНДА</td><td>ОЧКИ</td><td>МАТЧЕЙ</td><td>Побед</td><td>НИЧЬИ</td><td>ПОРАЖЕНИЯ</td></tr></thead>
+                <thead><tr><td>МЕСТО</td><td>КОМАНДА</td><td>ОЧКИ</td><td>ИГР</td><td>ПОБ</td><td>НИЧ</td><td>ПОР</td></tr></thead>
                 {this.props.ladder.teams.map((team) => (<tr class="ladder-block-wrapper">
                     <td>{team.place}</td><td><a href={'team/' + team.team.id}><img class="img-sm" src={team.team.logo}></img></a></td><td>{team.points}</td><td>{team.wins + team.loses + team.draws}</td><td>{team.wins}</td><td>{team.draws}</td><td>{team.loses}</td>
                 </tr>)
