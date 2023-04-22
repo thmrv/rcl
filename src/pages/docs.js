cf4957653469c09b94e7d0b86f882aa617e84534
v.tikhomirov <v.tikhomirov@whitewill.ru>
2023-04-22 10:10:43 +0300

    Обновление

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -39,17 +39,17 @@ function Docs() {
             <div class="title_lg">Документы</div>
             <div class="caption-md">Сезон 2022 – 2023</div>
             <div class="docs-wrapper">
-                <a href="https://ruscyberleague.ru/static/Reglament_Sorevnovaniya_RCL_2023.pdf" class="title_md">Регламент совернования РКЛ Сезон 2023</a>
+                <a href="./Reglament_Sorevnovaniya_RCL_2023.pdf" class="title_md">Регламент совернования РКЛ Сезон 2023</a>
                 <div class="caption-sm">3239 Кб</div>
-                <a href="https://ruscyberleague.ru/static/Disciplinarniy_reglament_RCL_2023.pdf" class="title_md">Дисциплинарный регламент РКЛ Сезон 2023</a>
+                <a href="./Disciplinarniy_reglament_RCL_2023.pdf" class="title_md">Дисциплинарный регламент РКЛ Сезон 2023</a>
                 <div class="caption-sm">650 Кб</div>
-                <a href="https://ruscyberleague.ru/static/Polojenie_o_disciplinarnom_komitete_RCL_2023.pdf" class="title_md">Положение о дисциплинарном комитете РКЛ Сезон 2023</a>
+                <a href="./Polojenie_o_disciplinarnom_komitete_RCL_2023.pdf" class="title_md">Положение о дисциплинарном комитете РКЛ Сезон 2023</a>
                 <div class="caption-sm">179 Кб</div>
-                <a href="https://ruscyberleague.ru/static/Polohenie_o_prizovom_fonde_RCL_2023.pdf" class="title_md">Положение о призовом фонде РКЛ Сезон 2023</a>
+                <a href="./Polohenie_o_prizovom_fonde_RCL_2023.pdf" class="title_md">Положение о призовом фонде РКЛ Сезон 2023</a>
                 <div class="caption-sm">2048 Кб</div>
-                <a href="https://ruscyberleague.ru/static/IESF_Statutes_2021.pdf" class="title_md">IESF Statutes 2021</a>
+                <a href="./IESF_Statutes_2021.pdf" class="title_md">IESF Statutes 2021</a>
                 <div class="caption-sm">726 Кб</div>
-                <a href="https://ruscyberleague.ru/static/CSGO_Rulebook.pdf" class="title_md">CSGO Rulebook</a>
+                <a href="./CSGO_Rulebook.pdf" class="title_md">CSGO Rulebook</a>
                 <div class="caption-sm">5801 Кб</div>
             </div>
         </div>)
