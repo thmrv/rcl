fd404703e7a2ab2f87569c8fd2368ddf1e9b96be
v.tikhomirov <v.tikhomirov@whitewill.ru>
2023-04-23 14:37:07 +0300

    Обновление: id в карточках команд

--- src/components/matchCard.js
+++ src/components/matchCard.js
@@ -7,32 +7,44 @@ export default class MatchCard extends React.Component {
     constructor(props) {
         super(props)
         this.dateTime = new Date();
+        this.teams = [];
     }
 
     render() {
         let self = this;
+        this.sortTeams();
         return (<div class="match-block-wrapper" id={'match' + this.props.match.id}>
-        {self.getDate(this.props.match.startedAt)}
-        <a class="match-link" href={'/match/' + this.props.match.id}>
-        <div class="left-wrapper">
-            <div class="date">{this.dateTime[1]}</div>
-            <div class="match-team-wrapper">
-                <img class="match-team-logo" src={this.props.teamPage ? this.props.match.teams[0].logo : this.props.match.team1.logo}></img>
-                <div class="match-team-title">{this.props.teamPage ? this.props.match.teams[0].name : this.props.match.team1.name}<div class="match-team-score">{this.props.pending ? '' : this.props.match.team1Score}</div></div>
-            </div>
-            <div class="match-vs">
-                <hr></hr>VS<hr></hr>
-            </div>
-            <div class="match-team-wrapper">
-                <img class="match-team-logo" src={this.props.teamPage ? this.props.match.teams[1].logo : this.props.match.team2.logo}></img>
-                <div class="match-team-title">{this.props.teamPage ? this.props.match.teams[1].name : this.props.match.team2.name}<div class="match-team-score">{this.props.pending ? '' : this.props.match.team2Score}</div></div>
-            </div>
-        </div>
-        <div class="right-wrapper time">
-            <div class="match-time">{this.props.pending ? this.dateTime[0] : 'Результат матча'}</div>
-        </div>
-        </a>
-    </div>)
+            {self.getDate(this.props.match.startedAt)}
+            <a class="match-link" href={'/match/' + this.props.match.id}>
+                <div class="left-wrapper">
+                    <div class="date">{this.dateTime[1]}</div>
+                    <div class="match-team-wrapper">
+                        <img class="match-team-logo" src={this.teams[0].logo}></img>
+                        <div class="match-team-title">{this.teams[0].name}<div class="match-team-score">{this.props.pending ? '' : this.props.match.team1Score}</div></div>
+                    </div>
+                    <div class="match-vs">
+                        <hr></hr>VS<hr></hr>
+                    </div>
+                    <div class="match-team-wrapper">
+                        <img class="match-team-logo" src={this.teams[1].logo}></img>
+                        <div class="match-team-title">{this.teams[1].name}<div class="match-team-score">{this.props.pending ? '' : this.props.match.team2Score}</div></div>
+                    </div>
+                </div>
+                <div class="right-wrapper time">
+                    <div class="match-time">{this.props.pending ? this.dateTime[0] : 'Результат матча'}</div>
+                </div>
+            </a>
+        </div>)
+    }
+
+    sortTeams() {
+        if (this.props.teamPage) {
+            this.teams.push(this.props.match.teams[0].id === this.props.match.team1Id ? this.props.match.teams[0] : this.props.match.teams[1]);
+            this.teams.push(this.props.match.teams[1].id === this.props.match.team2Id ? this.props.match.teams[1] : this.props.match.teams[0]);
+        } else {
+            this.teams.push(this.props.match.team1.id === this.props.match.team1Id ? this.props.match.team1 : this.props.match.team1);
+            this.teams.push(this.props.match.team2.id === this.props.match.team2Id ? this.props.match.team2 : this.props.match.team2);
+        }
     }
 
     getDate(date) {
